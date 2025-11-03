1e669c020b2e15cd7710b0f4a5311a32a02b25bd
ZavierZan <thethtutzan07@gmail.com>
2025-11-03 18:49:52 +1300

    tester

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={require('./star.png')} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am the world
         </p>
         <a
           className="App-link"
